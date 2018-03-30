f9e746b6a808e17e80a4df0b5d363b006dc5f5e0
jolrobles <jorgeluis96@live.com>
2018-03-30 14:28:43 -0500

    system technology run file

--- Tecnocity/settings.py
+++ Tecnocity/settings.py
@@ -137,3 +137,17 @@ STATIC_ROOT =os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env) 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
